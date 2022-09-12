9f2edad32a96fdd543749c7e4f29bf69a7c3fcc5
Aivils Štoss <aivils@ithouse.lv>
2022-09-12 11:45:11 +0300

    refactoring

--- src/s3-router.js
+++ src/s3-router.js
@@ -1,41 +1,83 @@
 const express = require('express');
 
 const { createPresignedPost } = require("@aws-sdk/s3-presigned-post");
-const { S3Client } = require('@aws-sdk/client-s3');
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const {
+  S3Client,
+  ListObjectsV2Command,
+  GetObjectCommand,
+} = require('@aws-sdk/client-s3');
 const { v4: uuidv4 } = require('uuid');
 
-const s3Client = new S3Client({ region: process.env.AWS_REGION });
+const client = new S3Client({ region: process.env.AWS_REGION });
 const s3Bucket = process.env.AWS_S3_BUCKET_NAME;
 
-async function generateUploadUrl({ type }) {
+const generateUploadUrl =  ({ type }) => {
   /**
    * We generate a new uuid as name, to prevent conflicting filenames.
    * You can install this package with `npm i uuid`
    */
   const name = uuidv4();
   const expiresInMinutes = 1;
-  return await createPresignedPost(s3Client, {
+  return createPresignedPost(client, {
     Bucket: s3Bucket,
     Key: `test-public/${name}`,
     Expires: expiresInMinutes * 60, // the url will only be valid for 1 minute
     Conditions: [["eq", "$Content-Type", type]],
   });
-}
+};
 
+const listObjects = () => {
+  const input = {
+    Bucket: s3Bucket,
+    Prefix: 'test-public/',
+  }
+  const command = new ListObjectsV2Command(input);
+  return client.send(command);
+};
+
+const getFileUrl = (Key) => {
+  const getObjectParams = {
+    Bucket: s3Bucket,
+    Key,
+  }
+  const command = new GetObjectCommand(getObjectParams);
+  return getSignedUrl(client, command, { expiresIn: 3600 })
+  .then((url) => {
+    const data = {};
+    data[Key] = url;
+    return Promise.resolve(data);
+  });
+};
 
 const router = express.Router();
 
-router.post("/generate-upload-url", async function (req, res) {
-  try {
-    const type = req.body.type;
-    if (!type) {
-      return res.status(400).json("invalid request body");
-    }
-    const data = await generateUploadUrl({ type });
-    return res.json(data);
-  } catch (e) {
-    return res.status(500).json(e.message);
+router.post("/generate-upload-url", (req, res) => {
+  const type = req.body.type;
+  if (!type) {
+    return res.status(400).json("invalid request body");
   }
+  generateUploadUrl({ type })
+  .then(data => {
+    return res.json(data);
+  })
+  .catch(e => {
+    return res.status(500).json(e.message);
+  });
+});
+
+router.get("/get-files", (req, res) => {
+  listObjects()
+  .then(async (data) => {
+    const { Contents } = data;
+    return Promise.all(Contents.map((item) => getFileUrl(item.Key)));
+  })
+  .then((data) => {
+    return res.json(data);
+  })
+  .catch(e => {
+    return res.status(500).json(e.message);
+  });
 });
 
 module.exports = router;
